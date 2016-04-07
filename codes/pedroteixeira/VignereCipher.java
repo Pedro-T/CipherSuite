3c615f7d6d2d4feab0fee96c8382cf63cb07fc0d
Pedro-T <pedroteixeira0@gmail.com>
2016-04-07 14:40:20 -0400

    Added decryption ability, documentation

--- codes/pedroteixeira/VignereCipher.java
+++ codes/pedroteixeira/VignereCipher.java
@@ -17,6 +17,14 @@ public class VignereCipher extends Cipher {
                 getPlainTextArea().setText(decrypt(getEncryptedTextArea().getText(), getKeyField().getText())));
     }
 
+    /**
+     * Encodes a string of text by looking up characters from the table according to key
+     * character and cipher text character
+     *
+     * @param input Text to be encrypted
+     * @param key Key string to use for encryption
+     * @return Encryped result
+     */
     private static String encrypt(String input, String key) {
         String text = cleanText(input);
         StringBuilder sb = new StringBuilder();
@@ -27,10 +35,28 @@ public class VignereCipher extends Cipher {
         return sb.toString();
     }
 
+    /**
+     * Decodes a string of text by looking up characters from the table according to key
+     * character and cipher text character
+     *
+     * @param input Text to be decrypted
+     * @param key Key string to use for decryption
+     * @return Decrypted result
+     */
     private static String decrypt(String input, String key) {
-        return input;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < input.length(); i++) {
+            int index = ALPHABET.indexOf(input.charAt(i)) - ALPHABET.indexOf(key.charAt(i%key.length()));
+            if (index < 0) {index += 26;}
+            sb.append(ALPHABET.charAt(index));
+        }
+        return sb.toString();
     }
 
+    /**
+     * Creates and returns a Tabula Rasa table to use for cipher operations
+     * @return Constructed table in 2D Array format
+     */
     private static char[][] getTableau() {
         char[][] table = new char[26][26];
         int shift = 0;
